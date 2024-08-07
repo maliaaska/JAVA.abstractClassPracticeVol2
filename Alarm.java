4d29c6e36e60f7c180fb44d4dc6b309c6914c7cc
maliaaska <asmilewski@gmail.com>
2024-08-07 11:52:19 +0200

    a default implementation added

--- Alarm.java
+++ Alarm.java
@@ -4,8 +4,14 @@ public interface Alarm {
     //Stwórz interfejs Alarm z dwiema domyślnymi metodami: turnAlarmOn() i turnAlarmOff(). Metody te powinny posiadać domyślną implementację.
     //Dodatkowo w interfejsie zadeklaruj stałą określającą jaki dźwięk wydaje alarm (typ String).
     public static final String sound = "Dryn Dryn";
-    void turnAlarmOn();
-    void turnAlarmOff();
-    // this is an testing text if a new branch has been created and pushed to github
+    default boolean turnAlarmOn() {
+        System.out.println("This is default implementation and the alarm gives a sound: " + sound + " !!!");
+        return true;
+    };
+    default boolean turnAlarmOff() {
+        System.out.println("A default implementation and the alarm is off.");
+        return false;
+    };
+
 
 }
