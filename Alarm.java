9cc6c846064b6600f4314a7720f8995c43a78637
maliaaska <asmilewski@gmail.com>
2024-08-07 11:53:01 +0200

    delation of redundunt line of codes

--- Alarm.java
+++ Alarm.java
@@ -3,7 +3,7 @@ package com.milewskiarkadiuszmodul12abstractTasks;
 public interface Alarm {
     //Stwórz interfejs Alarm z dwiema domyślnymi metodami: turnAlarmOn() i turnAlarmOff(). Metody te powinny posiadać domyślną implementację.
     //Dodatkowo w interfejsie zadeklaruj stałą określającą jaki dźwięk wydaje alarm (typ String).
-    public static final String sound = "Dryn Dryn";
+    String sound = "Dryn Dryn";
     default boolean turnAlarmOn() {
         System.out.println("This is default implementation and the alarm gives a sound: " + sound + " !!!");
         return true;
@@ -12,6 +12,4 @@ public interface Alarm {
         System.out.println("A default implementation and the alarm is off.");
         return false;
     };
-
-
 }
